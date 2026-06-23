51cc0d0dd271990ca8a6e531bcafe8f6a783a632
CaioTBarbieri <caiotbbarbieri@gmail,com>
2026-06-23 16:54:14 -0300

    Fix: Corrigindo erro de banco de dados ao salvar carro

--- api/src/main/java/com/ignitionmotors/api/models/Car.java
+++ api/src/main/java/com/ignitionmotors/api/models/Car.java
@@ -42,7 +42,6 @@ public class Car {
     @ElementCollection
     @CollectionTable(name = "car_images", joinColumns = @JoinColumn(name = "car_id"))
     @Column(name = "image_url", columnDefinition = "TEXT")
-    @Lob
     private List<String> imageUrls = new java.util.ArrayList<>(); // Nova lista de imagens
 
 
